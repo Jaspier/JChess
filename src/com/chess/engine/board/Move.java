d2c0c88e9f85a05914e4561ac0d500aa60b0d0c8
Jaspier <dungoallen349@gmail.com>
2023-12-22 17:03:57 +0000

    Starting to build the move 'execute' method

--- src/com/chess/engine/board/Move.java
+++ src/com/chess/engine/board/Move.java
@@ -1,5 +1,6 @@
 package com.chess.engine.board;
 
+import com.chess.engine.board.Board.Builder;
 import com.chess.engine.pieces.Piece;
 
 public abstract class Move {
@@ -42,7 +43,24 @@ public abstract class Move {
 
         @Override
         public Board execute() {
-            return null;
+
+            final Builder builder = new Builder();
+
+            for (final Piece piece : this.board.currentPlayer().getActivePieces()) {
+                // TODO: hashcode an 'equals' for pieces
+                if (!this.movedPiece.equals(piece)) {
+                    builder.setPiece(piece);
+                }
+            }
+
+            for (final Piece piece : this.board.currentPlayer().getOpponent().getActivePieces()) {
+                builder.setPiece(piece);
+            }
+            // TODO: Move the moved piece...
+            builder.setPiece(null);
+            builder.setMoveMaker(this.board.currentPlayer().getOpponent().getAlliance());
+
+            return builder.build();
         }
     }
 
