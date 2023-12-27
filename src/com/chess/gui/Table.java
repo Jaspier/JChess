f397f6a427127ef6ee7b3a41082d15a8b0fca8fc
Jaspier <dungoallen349@gmail.com>
2023-12-27 16:03:01 +0000

    Displaying the checkered board in the gui

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -1,26 +1,45 @@
 package com.chess.gui;
 
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Tile;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Table {
     private final JFrame gameFrame;
-    private static final Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
+    private final BoardPanel boardPanel;
+
+    private final Color lightTileColour = new Color(244, 230, 191);
+    private final Color darkTileColour = new Color(34, 139, 34);
+
+    private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
+    private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
+    private final static Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
 
     public Table() {
         this.gameFrame = new JFrame("JChess");
-        final JMenuBar tableMenuBar = new JMenuBar();
-        populateMenuBar(tableMenuBar);
+        this.gameFrame.setLayout(new BorderLayout());
+        final JMenuBar tableMenuBar = createTableMenuBar();
         this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
+
+        this.boardPanel = new BoardPanel();
+        this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
+
         this.gameFrame.setVisible(true);
 
     }
 
-    private void populateMenuBar(final JMenuBar tableMenuBar) {
+    private JMenuBar createTableMenuBar() {
+        final JMenuBar tableMenuBar = new JMenuBar();
         tableMenuBar.add(createFileMenu());
+
+        return tableMenuBar;
     }
 
     private JMenu createFileMenu() {
@@ -35,5 +54,47 @@ public class Table {
         fileMenu.add(openPGN);
 
         return fileMenu;
+    }
+
+    private class BoardPanel extends JPanel {
+        final List<TilePanel> boardTiles;
+
+        BoardPanel() {
+            super(new GridLayout(8,8));
+            this.boardTiles = new ArrayList<>();
+            for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
+                final TilePanel tilePanel = new TilePanel(this, i);
+                this.boardTiles.add(tilePanel);
+                add(tilePanel); // adding to JPanel
+            }
+            setPreferredSize(BOARD_PANEL_DIMENSION);
+            validate();
+        }
+    }
+
+    private class TilePanel extends JPanel {
+        private final int tileId;
+        TilePanel(final BoardPanel boardPanel, final int tileId) {
+            super(new GridBagLayout());
+            this.tileId = tileId;
+            setPreferredSize(TILE_PANEL_DIMENSION);
+            assignTileColour();
+            validate();
+        }
+
+        private void assignTileColour() {
+            if (BoardUtils.FIRST_ROW[this.tileId] ||
+                BoardUtils.THIRD_ROW[this.tileId] ||
+                    BoardUtils.FIFTH_ROW[this.tileId] ||
+                    BoardUtils.SEVENTH_ROW[this.tileId]) {
+                setBackground(this.tileId % 2 == 0 ? lightTileColour : darkTileColour);
+            } else if (BoardUtils.SECOND_ROW[this.tileId] ||
+                        BoardUtils.FOURTH_ROW[this.tileId] ||
+                        BoardUtils.SIXTH_ROW[this.tileId] ||
+                        BoardUtils.EIGHTH_ROW[this.tileId]) {
+                setBackground(this.tileId % 2 != 0 ? lightTileColour : darkTileColour);
+            }
+
+        }
     }
 }
